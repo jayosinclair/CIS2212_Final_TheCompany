aae1afdad03c27d1dca4347bdac80abeb4229ae5
josinclair <jay.olson@sinclair.edu>
2026-04-06 23:19:41 -0400

    Created Repo, Added Starter Files

---
+++ Program.java
@@ -0,0 +1,10 @@
+﻿public class Program
+    {
+        public static void main(String[] args)
+        {
+            PayrollManager pm = new PayrollManager("data.txt");
+            if (pm.loadData()) {
+               pm.runPayroll();
+            }
+        }
+    }
