f5f7a56f793a8c8ad3c0acf1e7d780a1a8a0d712
josinclair <jay.olson@sinclair.edu>
2026-04-07 22:26:49 -0400

    Started work on payroll manager.

--- PayrollManager.java
+++ PayrollManager.java
@@ -13,14 +13,86 @@ This program is intended to demonstrate mastery of CIS 2212 course concepts as t
 //**********************************************************************************************************************
 
 
+import java.io.FileInputStream;
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class PayrollManager {
     
     private String fileName;
-    private ArrayList <StaffMember> staffList; //TODO: This may or may not be the right container type. Keep reading spec...
+    private ArrayList <StaffMember> staffList = new ArrayList <StaffMember>(); //TODO: This may or may not be the right container type. Keep reading spec...
 
-    
 
+    public PayrollManager(String fileNm){
+
+        this.fileName = fileNm;
+
+    }
+
+
+    public boolean loadData(){
+
+        boolean fileFound = false;
+        Scanner keyboardScnr = new Scanner(System.in);
+
+
+        //Open file, handle exception
+
+
+        while (!fileFound){
+            
+            try {
+                
+                FileInputStream fileStream = new FileInputStream(fileName);
+                Scanner fScan = new Scanner(fileStream);
+
+                System.out.println("Loading data...");
+                fileFound = true;
+
+            } 
+            
+            catch (FileNotFoundException fnfe) {
+
+                System.out.println("File not found!");
+                return false;
+
+            }
+
+
+        }
+
+
+        //This entire implementation is flawed (need to create new objects to insert into arraylist), but to get started...
+        while (fScan.nextLine()){
+
+            if (fScan.nextLine().equals("Volunteer")){
+
+                staffList.add(Volunteer(fScan.nextLine(), fScan.nextLine(), fScan.nextLine()));
+                
+            }
+
+            else if (fScan.nextLine().equals("Hourly")){
+
+                staffList.add(Hourly(fScan.nextLine(), fScan.nextLine(), fScan.nextLine(), fScan.nextDouble()));
+                
+            }
+
+            //else if (fScan.nextLine().equals("Executive")){
+
+            else{
+
+                staffList.add(Hourly(fScan.nextLine(), fScan.nextLine(), fScan.nextLine(), fScan.nextLine(), fScan.nextDouble()));
+                
+            }
+
+
+        }
+
+        //Read file
+        //Close file
+
+
+    }
 
 }
